00bed9a47ae608260c7077b8fdfbdd5a7dde970f
ck1962 <ckeslin@gmail.com>
2019-09-30 18:16:09 -0500

    updated instructions in class

--- Todos/src/app/app.component.ts
+++ Todos/src/app/app.component.ts
@@ -1,32 +1,45 @@
 import { Component } from '@angular/core';
+import { ITodo}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-
 })
 export class AppComponent {
-  title = 'Todos';
-  todoList: any [] = [];
+  title = "Todos";
+  todoList: ITodo [] = [];
   todoTitle: string;
+  todoId: number;
 
   ngOnInit() {
-    this.todoTitle = '';
+    this.todoId = 1;
+    this.todoTitle = "";
     this.todoList = [
       // example of how to make an item in todo list
-      { title: 'Install Angular CLI', isDone: false },
+      {
+        id: this.todoId,
+        isDoing: false,
+        isEditing: false,
+        title: 'Install Angular CLI',
+        isDone: false,
+      dateAdded: new Date() }
 
     ];
   }
 // adds a todo to our list
 addTodo():void {
+  this.todoId++;
+
   this.todoList.push({
     title: this.todoTitle,
-    isDone: false
+    isDone: false,
+    isDoing: false,
+    isEditing: false,
+    dateAdded: new Date()
   });
    // resets our todoTitle variable to an empty string
-   this.todoTitle = '';
+   this.todoTitle = "";
 }
 
   // a method to delete an item
--- Todos/src/app/interfaces/itodo.ts
+++ Todos/src/app/interfaces/itodo.ts
@@ -1,2 +1,8 @@
 export interface ITodo {
-}
+    id: number;
+    title: string;
+    isDone: boolean;
+    isDoing: boolean;
+    isEditing: boolean;
+    dateAdded: Date;
+  }
